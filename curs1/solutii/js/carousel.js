8e02b3565a36a1957071fc8687bf97e3492a6719
aadiaconitei <aadiaconitei@yahoo.com>
2023-02-28 19:55:51 +0200

    curs 1

--- curs1/solutii/js/carousel.js
+++ curs1/solutii/js/carousel.js
@@ -2,7 +2,11 @@ var myCarousel = document.querySelector('#myCarousel')
 var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 2000,
   wrap: true,
-  pause: 'hover', //false
+  pause: false, //'hover'
   keyboard:true,
-  touch: true
+  touch: true,
+  ride: false
+});
+carousel.on('slide.bs.carousel', function () {
+  alert(1);
 })
